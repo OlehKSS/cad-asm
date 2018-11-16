9e8da3e42ef1840443c4995506fe825a2cd06553
briannaburton1 <briannaburton@gmail.com>
2018-11-16 16:24:37 +0100

    Updated readme and instructions for find_shape and find_hausdorff

--- examples/find_shape.py
+++ examples/find_shape.py
@@ -8,18 +8,19 @@ from ashapes.utils import no_points
 
 
 # Finds average shape, builds active shapes model and tests it
-# Path to hand data
+
+# Set paths to shapes.txt file, images folder, and specify test image
 path_shapes = "./data/hand/shapes/shapes.txt"
-path_shapes_norm = "./data/hand/shapes/shapes_norm.txt"
 path_images = "./data/hand/images"
 test_img_name = 4
 
 test_img_path = os.path.join(path_images, str(test_img_name).zfill(4) + ".jpg")
 
-shapes_norm = align(path_shapes, path_images, path_shapes_norm, test_img_name)
+shapes_norm, _ = align(path_shapes, path_images, test_img_name)
 new_shape = match_average_shape(shapes_norm, test_img_path, plot_results=True)
 
 img = cv2.imread(test_img_path, 0)
 plt.imshow(img, cmap="gray")
+plt.title("Final Result")
 plt.plot(new_shape[:no_points], new_shape[no_points:])
 plt.show()
